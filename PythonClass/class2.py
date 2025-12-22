fa03c45af3675de3572c771a92714151c536417d
samrato <willingtonejumamconyango@gmail.com>
2025-12-22 22:47:11 +0300

    A practice lang

--- PythonClass/class2.py
+++ PythonClass/class2.py
@@ -1,18 +1,82 @@
-# def lastCharater(text):
-#     return len(text) > 0 and text[-1] == ";"
+# # # def lastCharater(text):
+# # #     return len(text) > 0 and text[-1] == ";"
      
-# print(lastCharater("hello;"))
-# Check if the string is not empty AND last character is a colon
-# a code that will add space to that if its passs option 1 using elif 
+# # # print(lastCharater("hello;"))
+# # # Check if the string is not empty AND last character is a colon
+# # # a code that will add space to that if its passs option 1 using elif 
 
 
 
-def Lastcharacer(text):
-    # Check if the last character is a colon
-    if len(text) > 0 and text[-1] == ";":
-        return text +" "  
-    elif len(text) == 0:
-        return text 
-    else:
-        return text  
-print(Lastcharacer("Hellojuma;"))
+# # def Lastcharacer(text):
+
+# #     # Check if the last character is a colon
+# #     if len(text) > 0 and text[0] == "=+:
+# #         return  text + "\t "
+# #     elif len(text) == 0:
+# #         return text 
+# #     else:
+# #         return text  
+# # print(Lastcharacer("Hellojuma;"))
+
+
+# print("hello")
+# name="Juma"
+# print(name.capitalize())
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print("hello this is the next line ",end='')
+# print("hello juma ")
+# a=10
+# b=20
+# if a==b:
+#     print("a is equal to be ")
+# else:
+#     print("its not equal to 10")   
+
+
+# # gender=input("Please can u input ur gender ")
+# # profit=input("Enter the profit u earn")
+# # age=input("Enter ur age")
+# # age=int(age)
+# # profit=int(profit)
+# # if gender== "M":
+# #     if age >26:
+# #         dis=profit*0.09
+# #         print(dis)
+# #     elif age <26:
+# #         dis=profit*0.23
+# #         print(dis)
+# #     else :
+# #         print("Incorrect loops ") 
+# # elif gender== "F":
+# #     if age >26:
+# #         dis=profit*0.09
+# #         print(dis)
+# #     elif age <26:
+# #         dis=profit*0.23
+# #         print(dis)
+# #     else :
+# #         print("Incorrect loops ")             
+# # else:
+# #     print("No lopps")
+
+# for i in range(10):
+#     print(i+10)
+juma=[1,2,3,45,6,77,8]
+print(max(juma))
+# for i in range(len(juma)):
+#     print(juma[i])
+
+
+# value=10
+# while value > 0:
+#     value=int(input("Enter a value "))
+
+# def odd( number):
+#     if number%2==0:
+#         print("Its even number")
+#     else:
+#         print("Its odd number ")    
+
+# odd(2)
